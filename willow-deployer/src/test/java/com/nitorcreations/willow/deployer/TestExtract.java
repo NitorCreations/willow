cf5a007d08e4b4b023dd79e15af01dec6b61e484
Pasi Niemi <pasi.niemi@nitorcreations.com>
2014-10-15 16:47:47 +0300

    Fix zip file permission problems

---
+++ willow-deployer/src/test/java/com/nitorcreations/willow/deployer/TestExtract.java
@@ -0,0 +1,24 @@
+package com.nitorcreations.willow.deployer;
+
+import static org.junit.Assert.*;
+
+import java.io.File;
+
+import org.junit.Test;
+
+import com.nitorcreations.willow.utils.MergeableProperties;
+
+public class TestExtract {
+	@Test
+	public void testExtract() {
+		MergeableProperties props = new MergeableProperties();
+		props.setProperty("deployer.download.url[]", "file:./target/test-classes/test.zip");
+		props.setProperty("deployer.download.url[last].extract.glob", "**");
+		props.setProperty("deployer.download.url[last].extract.root", "target/test-extract");
+		PreLaunchDownloadAndExtract dwnld = new PreLaunchDownloadAndExtract(props);
+		dwnld.call();
+		File res = new File("target/test-extract/bin/graceful.sh");
+		assertTrue(res.exists());
+		assertTrue(res.canExecute());
+	}
+}
