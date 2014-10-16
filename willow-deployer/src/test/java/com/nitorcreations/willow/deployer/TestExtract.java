5d793461bf33bde76e3585749b6fedd241b71489
Pasi Niemi <pasi.niemi@nitorcreations.com>
2014-10-17 00:08:41 +0300

    Small fixes

--- willow-deployer/src/test/java/com/nitorcreations/willow/deployer/TestExtract.java
+++ willow-deployer/src/test/java/com/nitorcreations/willow/deployer/TestExtract.java
@@ -15,9 +15,15 @@ public class TestExtract {
 		props.setProperty("deployer.download.url[]", "file:./target/test-classes/test.zip");
 		props.setProperty("deployer.download.url[last].extract.glob", "**");
 		props.setProperty("deployer.download.url[last].extract.root", "target/test-extract");
+		props.setProperty("deployer.download.url[]", "file:./target/test-classes/test.zip");
+		props.setProperty("deployer.download.url[last].extract.glob", "**");
+		props.setProperty("deployer.download.url[last].extract.root", "target/test2-extract");
 		PreLaunchDownloadAndExtract dwnld = new PreLaunchDownloadAndExtract(props);
 		dwnld.call();
 		File res = new File("target/test-extract/bin/graceful.sh");
+		assertTrue(res.exists());
+		assertTrue(res.canExecute());
+		res = new File("target/test2-extract/bin/cm");
 		assertTrue(res.exists());
 		assertTrue(res.canExecute());
 	}
