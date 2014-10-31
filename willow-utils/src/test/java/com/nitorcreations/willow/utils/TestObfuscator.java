010f83aac3c9964c12ec25a4e9560f1843283401
Pasi Niemi <pasi.niemi@nitorcreations.com>
2014-10-31 11:11:07 +0200

    Add 256-bit AES support

---
+++ willow-utils/src/test/java/com/nitorcreations/willow/utils/TestObfuscator.java
@@ -0,0 +1,23 @@
+package com.nitorcreations.willow.utils;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import com.nitorcreations.willow.utils.Obfuscator.KeyDigest;
+
+public class TestObfuscator {
+
+	@Test
+	public void testObfuscate() {
+		String text = "FooBarBaz";
+		Obfuscator obf = new Obfuscator("MyKey");
+		assertEquals(text, obf.decrypt(obf.encrypt(text)));
+	}
+	@Test
+	public void testObfuscate2() {
+		String text = "FooBarBaz";
+		Obfuscator obf = new Obfuscator("MyKey", KeyDigest.SHA_256, 1000);
+		assertEquals(text, obf.decrypt(obf.encrypt(text)));
+	}
+}
