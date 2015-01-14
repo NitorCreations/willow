9868ce8a04e99e57a9aff5e3df6a7a28e1c38f88
Pasi Niemi <pasi.niemi@nitorcreations.com>
2015-01-14 11:26:36 +0200

    No need to synchronize - just change the order

--- willow-deployer/src/main/java/com/nitorcreations/willow/deployer/FileUtil.java
+++ willow-deployer/src/main/java/com/nitorcreations/willow/deployer/FileUtil.java
@@ -14,8 +14,8 @@ import com.nitorcreations.willow.utils.ReplaceTokensInputStream;
 
 public class FileUtil {
 	public static final int BUFFER_LEN = 8 * 1024;
-	public static synchronized boolean createDir(File dir) {
-		return dir.exists() || dir.mkdirs();
+	public static boolean createDir(File dir) {
+		return  dir.mkdirs() || dir.exists();
 	}
 	public static String getFileName(String name) {
 		int lastSeparator = Math.max(name.lastIndexOf('/'), name.lastIndexOf('\\'));
