7220f6c58e4a6f47b74e8d8ce6e6f1db46cb5bc9
Pasi Niemi <pasi.niemi@nitorcreations.com>
2015-01-16 14:21:17 +0200

    Clean up and add logging

--- willow-deployer/src/main/java/com/nitorcreations/willow/deployer/PreLaunchDownloadAndExtract.java
+++ willow-deployer/src/main/java/com/nitorcreations/willow/deployer/PreLaunchDownloadAndExtract.java
@@ -1,47 +1,18 @@
 package com.nitorcreations.willow.deployer;
 
-import static com.nitorcreations.willow.deployer.PropertyKeys.PROPERTY_KEY_DOWNLOAD_DIRECTORY;
 import static com.nitorcreations.willow.deployer.PropertyKeys.PROPERTY_KEY_PREFIX_DOWNLOAD_ARTIFACT;
 import static com.nitorcreations.willow.deployer.PropertyKeys.PROPERTY_KEY_PREFIX_DOWNLOAD_URL;
-import static com.nitorcreations.willow.deployer.PropertyKeys.PROPERTY_KEY_SUFFIX_DOWNLOAD_FINALPATH;
 import static com.nitorcreations.willow.deployer.PropertyKeys.PROPERTY_KEY_SUFFIX_DOWNLOAD_MD5;
 import static com.nitorcreations.willow.deployer.PropertyKeys.PROPERTY_KEY_SUFFIX_DOWNLOAD_IGNORE_MD5;
-import static com.nitorcreations.willow.deployer.PropertyKeys.PROPERTY_KEY_SUFFIX_EXTRACT_FILTER_GLOB;
-import static com.nitorcreations.willow.deployer.PropertyKeys.PROPERTY_KEY_SUFFIX_EXTRACT_GLOB;
-import static com.nitorcreations.willow.deployer.PropertyKeys.PROPERTY_KEY_SUFFIX_EXTRACT_ROOT;
-import static com.nitorcreations.willow.deployer.PropertyKeys.PROPERTY_KEY_SUFFIX_EXTRACT_SKIP_GLOB;
-import static com.nitorcreations.willow.deployer.PropertyKeys.PROPERTY_KEY_SUFFIX_EXTRACT_OVERWRITE;
-import static com.nitorcreations.willow.deployer.PropertyKeys.PROPERTY_KEY_WORKDIR;
 
-import java.io.BufferedInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 import java.net.URL;
 import java.net.URLConnection;
-import java.nio.file.FileSystem;
-import java.nio.file.FileSystems;
-import java.nio.file.Files;
-import java.nio.file.PathMatcher;
-import java.nio.file.Paths;
-import java.nio.file.attribute.PosixFileAttributeView;
-import java.nio.file.attribute.PosixFilePermission;
-import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Enumeration;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.LinkedHashSet;
 import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
 import java.util.Properties;
-import java.util.Set;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
@@ -53,17 +24,6 @@ import java.util.logging.Logger;
 
 import org.apache.commons.codec.DecoderException;
 import org.apache.commons.codec.binary.Hex;
-import org.apache.commons.compress.archivers.ArchiveEntry;
-import org.apache.commons.compress.archivers.ArchiveException;
-import org.apache.commons.compress.archivers.ArchiveInputStream;
-import org.apache.commons.compress.archivers.ArchiveStreamFactory;
-import org.apache.commons.compress.archivers.zip.ZipArchiveEntry;
-import org.apache.commons.compress.archivers.zip.ZipExtraField;
-import org.apache.commons.compress.archivers.zip.ZipFile;
-import org.apache.commons.compress.compressors.CompressorException;
-import org.apache.commons.compress.compressors.CompressorStreamFactory;
-
-import com.nitorcreations.willow.utils.MD5SumInputStream;
 
 public class PreLaunchDownloadAndExtract implements Callable<Integer> {
 	private final Properties properties;
@@ -74,23 +34,25 @@ public class PreLaunchDownloadAndExtract implements Callable<Integer> {
 	public Integer call() {
 		ExecutorService executor = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors() * 2);
 		int downloads = 0;
-		int i=0;
-		String nextUrl=null;
 		List<Future<Boolean>> futures = new ArrayList<>();
-		while (null != (nextUrl = properties.getProperty(PROPERTY_KEY_PREFIX_DOWNLOAD_URL + "[" + i + "]"))) {
+		for (int i=0; null != (properties.getProperty(PROPERTY_KEY_PREFIX_DOWNLOAD_URL + "[" + i + "]")); i++) {
 			final String index = "[" + i + "]";
 			Future<Boolean> next = executor.submit(new Callable<Boolean>() {
 				@Override
 				public Boolean call() throws Exception {
-					UrlDownloader dwn = new UrlDownloader(properties, index, getMd5(properties, index));
-					return new Extractor(properties, index, PROPERTY_KEY_PREFIX_DOWNLOAD_URL, dwn.call()).call();
+					byte[] md5 = getMd5(properties, index);
+					UrlDownloader dwn = new UrlDownloader(properties, index, md5);
+					File downloaded = dwn.call();
+					if (downloaded != null && downloaded.exists()) {
+						return new Extractor(properties, index, PROPERTY_KEY_PREFIX_DOWNLOAD_URL, downloaded).call();
+					} else {
+						return false;
+					}
 				}
 			});
 			futures.add(next);
-			i++;
 		}
-		i = 0;
-		while (null != (nextUrl = properties.getProperty(PROPERTY_KEY_PREFIX_DOWNLOAD_ARTIFACT + "[" + i + "]"))) {
+		for (int i=0; null != (properties.getProperty(PROPERTY_KEY_PREFIX_DOWNLOAD_ARTIFACT + "[" + i + "]")); i++) {
 			final String index = "[" + i + "]";
 			Future<Boolean> next = executor.submit(new Callable<Boolean>() {
 				@Override
@@ -100,7 +62,6 @@ public class PreLaunchDownloadAndExtract implements Callable<Integer> {
 				}
 			});
 			futures.add(next);
-			i++;
 		}
 		boolean failures = false;
 		for (Future<Boolean> next : futures) {
@@ -108,6 +69,9 @@ public class PreLaunchDownloadAndExtract implements Callable<Integer> {
 				downloads++;
 				if (!next.get()) failures = true;
 			} catch (InterruptedException | ExecutionException e) {
+				LogRecord rec = new LogRecord(Level.WARNING, "Failed download and extract");
+				rec.setThrown(e);
+				logger.log(rec);
 			}
 		}
 		executor.shutdownNow();
