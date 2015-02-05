29cc7a53a5a04d819aad2a776277dec4a3d784ef
Pasi Niemi <pasi.niemi@nitorcreations.com>
2015-02-05 19:54:12 +0200

    Add rpm for deployer

--- willow-deployer/src/main/shell/create-script.sh
+++ willow-deployer/src/main/shell/create-script.sh
@@ -4,4 +4,5 @@ MD5=$(md5sum target/willow-deployer-*-jar-with-dependencies.jar | cut -d " " -f 
 ARCHIVE_START=$(($(wc -l  src/main/shell/deployer.sh | cut -d" " -f1) + 1))
 sed -e "s/%%MD5%%/$MD5/" -e "s/%%ARCHIVE_START%%/$ARCHIVE_START/" src/main/shell/deployer.sh > target/deployer.sh
 cat target/willow-deployer-*-jar-with-dependencies.jar >> target/deployer.sh
+ln target/willow-deployer-*-jar-with-dependencies.jar target/deployer-uber-$MD5.jar
 chmod 755 target/deployer.sh
