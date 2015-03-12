b32c5eda8e8be4694998589e36c0242bd572f67a
Pasi Niemi <pasi.niemi@nitorcreations.com>
2015-03-12 16:20:01 +0200

    Add EnumerationIterable utility

---
+++ willow-utils/src/main/java/com/nitorcreations/willow/utils/EnumerationIterable.java
@@ -0,0 +1,26 @@
+package com.nitorcreations.willow.utils;
+
+import java.util.Enumeration;
+import java.util.Iterator;
+
+public class EnumerationIterable<T> implements Iterable<T> {
+  private final Enumeration<T> enumeration;
+
+  public EnumerationIterable(Enumeration<T> enumeration) {
+    this.enumeration = enumeration;
+  }
+
+  @Override
+  public Iterator<T> iterator() {
+    return new Iterator<T>() {
+    	@Override
+  		public boolean hasNext() {
+	    	return enumeration.hasMoreElements();
+	    }
+  		@Override
+  		public T next() {
+  			return enumeration.nextElement();
+  		}
+  	};
+  }
+}
