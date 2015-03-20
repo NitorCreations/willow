2467a99be752b69e63d16d49de1ef9913fe98c6b
Pasi Niemi <pasi.niemi@nitorcreations.com>
2015-03-20 18:47:30 +0200

    Refator file polling to a dedicated class and build polling properties on top of that

--- willow-utils/src/main/java/com/nitorcreations/willow/utils/FilePollingProperties.java
+++ willow-utils/src/main/java/com/nitorcreations/willow/utils/FilePollingProperties.java
@@ -4,18 +4,16 @@ import static java.nio.file.StandardWatchEventKinds.*;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.nio.file.FileSystems;
 import java.nio.file.Path;
-import java.nio.file.WatchEvent;
-import java.nio.file.WatchKey;
-import java.nio.file.WatchService;
+import java.nio.file.WatchEvent.Kind;
 import java.util.Map.Entry;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class FilePollingProperties {
+import com.nitorcreations.willow.utils.PollingFile.FileListener;
+
+public class FilePollingProperties implements FileListener {
   public interface PropertyChangeListerner {
     public void propertyValueChanged(String key, String newValue, String oldValue);
     public void propertyAdded(String key, String value);
@@ -28,6 +26,7 @@ public class FilePollingProperties {
       this.log = Logger.getLogger(loggerName);
       this.level = level;
     }
+    
     @Override
     public void propertyValueChanged(String key, String newValue, String oldValue) {
       log.log(level, "CHANGE: " + key + " = " + oldValue + " => " + newValue);
@@ -43,92 +42,66 @@ public class FilePollingProperties {
       log.log(level, "ADDED: " + key + " = " + value);
     }
   }
+  private final PropertyChangeListerner listener;
   private final MergeableProperties properties;
 
-  public FilePollingProperties(final File source) {
+  public FilePollingProperties(final String source) {
     this(source, new LoggingPropertyChangeListener(FilePollingProperties.class.getName(), Level.FINE));
   }
-  public FilePollingProperties(final File source, final PropertyChangeListerner listener) {
+  public FilePollingProperties(final String source, final PropertyChangeListerner listener) {
     properties = new MergeableProperties();
+    this.listener = listener;
     try {
       properties.load(new FileInputStream(source));
     } catch (IOException e) {
       //Noop
     }
-    final Path sourcePath = source.getAbsoluteFile().getParentFile().toPath();
-    try {
-      final WatchService watcher = FileSystems.getDefault().newWatchService();
-      sourcePath.register(watcher, ENTRY_MODIFY, ENTRY_CREATE, ENTRY_DELETE);
-      new Thread(new Runnable() {
-        @Override
-        public void run() {
-          for (;;) {
-            WatchKey key = null;
-            try {
-              key = watcher.take();
-            } catch (InterruptedException x) {
-              return;
-            }
-            for (WatchEvent<?> event: key.pollEvents()) {
-              WatchEvent.Kind<?> kind = event.kind();
-              if (kind == OVERFLOW) {
-                  continue;
-              }
-              @SuppressWarnings("unchecked")
-              WatchEvent<Path> ev = (WatchEvent<Path>)event;
-              Path context = ev.context();
-              if (!context.equals(source.toPath())) {
-                continue;
-              }
-              if (kind == ENTRY_DELETE) {
-                for (Entry<String, String> next : properties.backingEntrySet()) {
-                  listener.propertyRemoved(next.getKey(), next.getValue());
-                }
-                properties.clear();
-              } else {
-                MergeableProperties newProps = new MergeableProperties();
-                try {
-                  newProps.load(new FileInputStream(source));
-                  MergeableProperties oldProps ;
-                  synchronized (properties) {
-                    oldProps = (MergeableProperties) properties.clone();
-                    properties.clear();
-                    properties.putAll(newProps);
-                  }
-                  for (Entry<String, String> next : newProps.backingEntrySet()) {
-                      if (oldProps.containsKey(next.getKey())) {
-                        if (!oldProps.getProperty(next.getKey()).equals(next.getValue())) {
-                          listener.propertyValueChanged(next.getKey(), next.getValue(), oldProps.getProperty(next.getKey()));
-                        }
-                        oldProps.remove(next.getKey());
-                      } else {
-                        listener.propertyAdded(next.getKey(), next.getValue());
-                      }
-                    }
-                    for (Entry<String, String> next : oldProps.backingEntrySet()) {
-                      listener.propertyRemoved(next.getKey(), next.getValue());
-                    }
-                } catch (IOException e) {
-                }
-              }
-            }
-            boolean valid = key.reset();
-            if (!valid) {
-              break;
-            }
-            continue;
-          }
-        }
-      }).start();
-    } catch (Exception e) {
-    }
+    PollingFile backend = new PollingFile(source, this);
+    backend.startListening();
   }
+  
   public MergeableProperties getProperties() {
     synchronized (properties) {
       return (MergeableProperties) properties.clone();
     }
   }
   public static void main(String[] args) throws IOException {
-    new FilePollingProperties(new File(args[0]), new LoggingPropertyChangeListener("main", Level.INFO));
+    new FilePollingProperties(args[0], new LoggingPropertyChangeListener("main", Level.INFO));
+  }
+  
+  @Override
+  public void fileChanged(File file, Kind<Path> kind) {
+    MergeableProperties oldProps ;
+    if (kind == ENTRY_DELETE) {
+      oldProps = (MergeableProperties) properties.clone();
+      properties.clear();
+      for (Entry<String, String> next : oldProps.backingEntrySet()) {
+        listener.propertyRemoved(next.getKey(), next.getValue());
+      }
+    } else {
+      MergeableProperties newProps = new MergeableProperties();
+      try {
+        newProps.load(new FileInputStream(file));
+        synchronized (properties) {
+          oldProps = (MergeableProperties) properties.clone();
+          properties.clear();
+          properties.putAll(newProps);
+        }
+        for (Entry<String, String> next : newProps.backingEntrySet()) {
+            if (oldProps.containsKey(next.getKey())) {
+              if (!oldProps.getProperty(next.getKey()).equals(next.getValue())) {
+                listener.propertyValueChanged(next.getKey(), next.getValue(), oldProps.getProperty(next.getKey()));
+              }
+              oldProps.remove(next.getKey());
+            } else {
+              listener.propertyAdded(next.getKey(), next.getValue());
+            }
+          }
+          for (Entry<String, String> next : oldProps.backingEntrySet()) {
+            listener.propertyRemoved(next.getKey(), next.getValue());
+          }
+      } catch (IOException e) {
+      }
+    }
   }
 }
---
+++ willow-utils/src/main/java/com/nitorcreations/willow/utils/PollingFile.java
@@ -0,0 +1,85 @@
+package com.nitorcreations.willow.utils;
+
+import static java.nio.file.StandardWatchEventKinds.*;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+import java.nio.file.WatchEvent;
+import java.nio.file.WatchKey;
+import java.nio.file.WatchService;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+public class PollingFile extends File implements Runnable {
+  private final AtomicBoolean running = new AtomicBoolean(true);
+  private Thread poller = null;
+  
+  public interface FileListener {
+    public void fileChanged(File file, WatchEvent.Kind<Path> kind);
+  }
+  private final FileListener listener;
+
+  public PollingFile(File parent, String child, FileListener listener) {
+    super(parent, child);
+    this.listener = listener;
+  }
+  public PollingFile(String name, FileListener listener) {
+    super(name);
+    this.listener = listener;
+  }
+  public PollingFile(String parent, String child, FileListener listener) {
+    super(parent, child);
+    this.listener = listener;
+  }
+  public PollingFile(URI uri, FileListener listener) {
+    super(uri);
+    this.listener = listener;
+  }
+  public void startListening() {
+    poller = new Thread(this, getAbsolutePath() + "-listener");
+    poller.start();
+  }
+  private static final long serialVersionUID = -6683198740670677087L;
+
+  @Override
+  public void run() {
+    try {
+      WatchService watcher = FileSystems.getDefault().newWatchService();
+      getAbsoluteFile().getParentFile().toPath().register(watcher, ENTRY_MODIFY, ENTRY_CREATE, ENTRY_DELETE);
+      while (running.get()) {
+        WatchKey key = null;
+        try {
+          key = watcher.take();
+        } catch (InterruptedException x) {
+          return;
+        }
+        for (WatchEvent<?> event: key.pollEvents()) {
+          WatchEvent.Kind<?> kind = event.kind();
+          if (kind == OVERFLOW) {
+            continue;
+          }
+          @SuppressWarnings("unchecked")
+          WatchEvent<Path> ev = (WatchEvent<Path>)event;
+          Path context = ev.context();
+          if (context.equals(toPath())) {
+            listener.fileChanged(this, ev.kind());
+          }
+        }
+        boolean valid = key.reset();
+        if (!valid) {
+          break;
+        }
+      }
+    } catch (IOException e) {
+      throw new RuntimeException("Failed to watch file changes", e);
+    }
+  }
+  public void stop() {
+    running.set(false);
+    if (poller != null) {
+      poller.interrupt();
+    }
+  }
+}
