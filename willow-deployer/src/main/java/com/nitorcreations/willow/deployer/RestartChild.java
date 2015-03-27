0ed25070451c3a65cc8d78aecdeecd15779e1fdf
Pasi Niemi <pasi.niemi@nitorcreations.com>
2015-03-27 15:54:18 +0200

    Add feature to restart all or one child

--- willow-deployer/src/main/java/com/nitorcreations/willow/deployer/RestartChild.java
+++ willow-deployer/src/main/java/com/nitorcreations/willow/deployer/RestartChild.java
@@ -14,9 +14,9 @@ import org.hyperic.sigar.Sigar;
 import org.hyperic.sigar.ptql.ProcessQuery;
 import org.hyperic.sigar.ptql.ProcessQueryFactory;
 
-public class Status extends DeployerControl {
+public class RestartChild extends DeployerControl {
   public static void main(String[] args) {
-    new Status().doMain(args);
+    new RestartChild().doMain(args);
   }
 
   public void doMain(String[] args) {
@@ -41,10 +41,19 @@ public class Status extends DeployerControl {
         try (JMXConnector conn = getJMXConnector(firstPid)) {
           MBeanServerConnection server = conn.getMBeanServerConnection();
           MainMBean proxy = JMX.newMBeanProxy(server, OBJECT_NAME, MainMBean.class);
+          if (args.length == 1) {
+            System.out.println("Restarting all children of " + args[0]);
+            proxy.restartChild(null);
+          } else {
+            for (int i=1; i<args.length;i++) {
+              System.out.println("Restarting '" + args[i] + "' child of " + args[0]);
+              proxy.restartChild(args[i]);
+            }
+          }
           System.out.println(proxy.getStatus());
           System.exit(0);
         } catch (Throwable e) {
-          log.info("JMX Status failed");
+          log.info("Restart failed");
           e.printStackTrace();
           System.exit(1);
         }
