49381ed20bbdc5588cc4bce6d00115685427a7fd
Pasi Niemi <pasi.niemi@nitorcreations.com>
2015-04-16 01:05:26 +0300

    Default servlet init params

--- willow-servers/src/main/java/com/nitorcreations/willow/servers/ApplicationServletModule.java
+++ willow-servers/src/main/java/com/nitorcreations/willow/servers/ApplicationServletModule.java
@@ -2,6 +2,8 @@ package com.nitorcreations.willow.servers;
 
 import static java.lang.System.getProperty;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -22,6 +24,13 @@ public class ApplicationServletModule extends ServletModule {
     bind(DefaultServlet.class).toInstance(new DefaultServlet());;
     bind(ExecutorService.class).toInstance(Executors.newCachedThreadPool());
 
+    Map<String, String> defaultInit = new HashMap<>();
+    defaultInit.put("dirAllowed", "false");
+    defaultInit.put("gzip", "false");
+    defaultInit.put("maxCacheSize","81920");
+    defaultInit.put("maxCacheSize","81920");
+    defaultInit.put("welcomeServlets", "true");
+
     if ("dev".equals(env)) {
       bind(ElasticsearchProxy.class);
       bind(TestServlet.class).toInstance(new TestServlet());;
@@ -32,6 +41,6 @@ public class ApplicationServletModule extends ServletModule {
     serve("/properties/*").with(PropertyServlet.class);
     serve("/statistics/*").with(StatisticsServlet.class);
     serve("/rawterminal/*").with(RawTerminalServlet.class);
-    serve("/*").with(DefaultServlet.class);
+    serve("/*").with(DefaultServlet.class, defaultInit);
   }
 }
