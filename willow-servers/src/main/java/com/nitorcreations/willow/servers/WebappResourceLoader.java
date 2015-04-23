77823c6e400c736f81b6be71d6edaf2aeec0f8f1
Pasi Niemi <pasi.niemi@nitorcreations.com>
2015-04-23 16:04:44 +0300

    Ui tweaks

--- willow-servers/src/main/java/com/nitorcreations/willow/servers/WebappResourceLoader.java
+++ willow-servers/src/main/java/com/nitorcreations/willow/servers/WebappResourceLoader.java
@@ -1,6 +1,10 @@
 package com.nitorcreations.willow.servers;
 
+import java.io.File;
 import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
+import java.net.URL;
 
 import javax.servlet.ServletContext;
 
@@ -11,44 +15,79 @@ import org.apache.velocity.runtime.resource.loader.ResourceLoader;
 
 public class WebappResourceLoader extends ResourceLoader {
 
-    public static final String NAME = "webapp";
+  public static final String NAME = "webapp";
 
-    private ServletContext servletContext;
+  private ServletContext servletContext;
 
-    @Override
-    public void init(ExtendedProperties configuration) {
-        final String scClassName = ServletContext.class.getName();
-        final Object sc = this.rsvc.getApplicationAttribute(scClassName);
-        if (sc instanceof ServletContext) {
-            this.servletContext = (ServletContext) sc;
-        } else {
-            this.log.error("Not found " + scClassName + ": " + sc);
-        }
+  @Override
+  public void init(ExtendedProperties configuration) {
+    final String scClassName = ServletContext.class.getName();
+    final Object sc = this.rsvc.getApplicationAttribute(scClassName);
+    if (sc instanceof ServletContext) {
+      servletContext = (ServletContext) sc;
+    } else {
+      log.error("Not found " + scClassName + ": " + sc);
+    }
+  }
+
+  @Override
+  public InputStream getResourceStream(final String source) throws ResourceNotFoundException {
+
+    if (source == null || source.length() == 0) {
+      throw new ResourceNotFoundException("No template name");
     }
 
-    @Override
-    public InputStream getResourceStream(final String source)
-            throws ResourceNotFoundException {
-
-        if (source == null || source.length() == 0) {
-            throw new ResourceNotFoundException("No template name");
-        }
-
-        final InputStream res = this.servletContext
-                .getResourceAsStream(source);
-        if (res == null) {
-            throw new ResourceNotFoundException(source);
-        }
-        return res;
+    final InputStream res = servletContext.getResourceAsStream(source);
+    if (res == null) {
+      throw new ResourceNotFoundException(source);
     }
+    return res;
+  }
 
-    @Override
-    public boolean isSourceModified(Resource resource) {
-        return resource.isSourceModified();
+  @Override
+  public boolean isSourceModified(Resource resource) {
+    try {
+      URL url = servletContext.getResource(resource.getName());
+      if (url == null) return true;
+      if (url.toString().startsWith("file:")) {
+        return urlToFile(url).lastModified() > resource.getLastModified();
+      } else if (url.toString().startsWith("jar:")) {
+        String sUrl = url.toString();
+        String fName = sUrl.split("\\!")[0].substring(4); 
+        return urlToFile(new URL(fName)).lastModified() > resource.getLastModified();
+      } else {
+        return true;
+      }
+    } catch (MalformedURLException e) {
+      return true;
     }
-
-    @Override
-    public long getLastModified(Resource resource) {
-        return resource.getLastModified();
+  }
+  private File urlToFile(URL url) {
+    File f;
+    try {
+      f = new File(url.toURI());
+    } catch(URISyntaxException e) {
+      f = new File(url.getPath());
     }
+    return f;
+  }
+  @Override
+  public long getLastModified(Resource resource) {
+    long now = System.currentTimeMillis();
+    try {
+      URL url = servletContext.getResource(resource.getName());
+      if (url == null) return now;
+      if (url.toString().startsWith("file:")) {
+        return urlToFile(url).lastModified();
+      } else if (url.toString().startsWith("jar:")) {
+        String sUrl = url.toString();
+        String fName = sUrl.split("\\!")[0].substring(4); 
+        return urlToFile(new URL(fName)).lastModified();
+      } else {
+        return now;
+      }
+    } catch (MalformedURLException | NullPointerException e) {
+      return now;
+    }
+  }
 }
