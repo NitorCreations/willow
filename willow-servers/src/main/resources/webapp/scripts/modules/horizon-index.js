2b172e1c12c39f1daae73cd7b26cdc1f2be35ffe
Sami Airaksinen <samiaira@gmail.com>
2015-05-04 21:26:02 +0300

    refactored horizon graph construction
    - each isolated element is created in own function
    - cubism context call is extracted to own function, can be moved to
      better place (service maybe?)
    - moved every confirution to metricsMap

--- willow-servers/src/main/resources/webapp/scripts/modules/horizon-index.js
+++ willow-servers/src/main/resources/webapp/scripts/modules/horizon-index.js
@@ -1,4 +1,4 @@
-Box.Application.addModule('horizon-index', function(context) {
+Box.Application.addModule('horizon-index', function(context) { //FIXME rename to cloud|summary|?|-index?
   'use strict';
 
   var moduleElement, windowSvc, d3, cubism, metric, timescale, utils, $, cubismContext, graphEnd, stop;
@@ -6,11 +6,11 @@ Box.Application.addModule('horizon-index', function(context) {
   var defaultColors = ["#08519c", "#3182bd", "#6baed6", "#bdd7e7", "#bae4b3", "#74c476", "#31a354", "#006d2c"];
   var cpuColors = ["#08519c", "#3182bd", "#6baed6", "#bdd7e7", "#bae4b3", "#006d2c", "#b07635", "#d01717"];
   var metricMap = {
-    "cpu" : { "title" : "cpu: ", "format" : ".2f", "extent": [0, 100], colors : cpuColors },
-    "mem" : { "title" : "mem: ", "format" : ".2f", "extent": [0, 100], colors : cpuColors },
-    "net" : { "title" : "net: ", "format" : ".2f", "extent": undefined, colors : defaultColors },
-    "diskio" : { "title" : "io: ", "format" : ".2f", "extent": undefined, colors : defaultColors },
-    "tcpinfo" : { "title" : "conn: ", "format" : ".0f", "extent": undefined, colors : defaultColors }
+    "cpu" : { "title" : "cpu: ", "format" : ".2f", "extent": [0, 100], colors : cpuColors, height: 50 },
+    "mem" : { "title" : "mem: ", "format" : ".2f", "extent": [0, 100], colors : cpuColors, height: 50 },
+    "net" : { "title" : "net: ", "format" : ".2f", "extent": undefined, colors : defaultColors, height: 50 },
+    "diskio" : { "title" : "io: ", "format" : ".2f", "extent": undefined, colors : defaultColors, height: 50 },
+    "tcpinfo" : { "title" : "conn: ", "format" : ".0f", "extent": undefined, colors : defaultColors, height: 50 }
   };
 
   var deployer_metric = function(name, tag, stop, step) {
@@ -93,29 +93,53 @@ Box.Application.addModule('horizon-index', function(context) {
       }
     });
   };
-  var createHorizon = function(div, host, chart, metricSettings) {
-    var graphDiv = div.selectAll(".horizon-" + host)
+
+  var createHorizon = function(parentElement, host, chart, metricSettings) {
+    var horizonGraphElements = parentElement.selectAll(".horizon-" + host)
         .data([chart])
         .enter().append("div");
-    graphDiv.attr("class", "horizon horizon-" + host + " horizoncpu-" + host)
+
+    horizonGraphElements.call(appendHorizonGraph, host, metricSettings);
+    horizonGraphElements.call(appendTerminalIcon, host);
+    horizonGraphElements.call(appendShareRadiatorIcon, host);
+
+    parentElement.call(appendDetailsScreen, host);
+  };
+
+  function appendHorizonGraph(parentElement, host, metricSettings) {
+    return parentElement
+        .classed("horizon horizon-" + host + " horizoncpu-" + host, true)
         .attr("data-host", host)
-        .call(cubismContext.horizon()
-            .height(50)
-            .colors(metricSettings.colors)
-            .extent(metricSettings.extent)
-            .format(d3.format(metricSettings.format))
-            .title(metricSettings.title + host));
-    graphDiv.append("svg").attr("viewBox", "0 0 124 124")
-        .attr("class", "icon shape-terminal terminal-" + host)
+        .call(configureHorizonGraph(host, metricSettings));
+  }
+
+  function appendTerminalIcon(parentElement, host) {
+    return parentElement.append("svg").attr("viewBox", "0 0 124 124")
+        .classed("icon shape-terminal terminal-" + host, true)
         .attr("data-type", "start-terminal")
         .attr("data-host", host)
-        .append("use").attr("xlink:href","#shape-terminal");
-    graphDiv.append("svg").attr("viewBox", "0 0 100 100")
-        .attr("class", "icon shape-share share-" + host)
-        .append("use").attr("xlink:href","#shape-to-radiator");
-    div.append("div")
-        .attr("class", "details details-" + host);
-  };
+        .append("use").attr("xlink:href", "#shape-terminal");
+  }
+
+  function appendShareRadiatorIcon(parentElement, host) {
+    return parentElement.append("svg").attr("viewBox", "0 0 100 100")
+        .classed("icon shape-share share-" + host, true)
+        .append("use").attr("xlink:href", "#shape-to-radiator");
+  }
+
+  function appendDetailsScreen(parentElement, host) {
+    return parentElement.append("div").classed("details details-" + host, true);
+  }
+
+  function configureHorizonGraph(host, metricSettings) {
+    return cubismContext.horizon()
+        .height(metricSettings.height)
+        .colors(metricSettings.colors)
+        .extent(metricSettings.extent)
+        .format(d3.format(metricSettings.format))
+        .title(metricSettings.title + host);
+  }
+
   return {
     init: function() {
       moduleElement = context.getElement();
