014ee72126693f0d3b2cc5feac8e3b6ddf672aad
Pasi Niemi <pasi.niemi@nitorcreations.com>
2015-05-04 15:30:22 +0300

    Add check for timestamp - needs to be in 30s window from now (+-15s) and needs to not have seen before

--- willow-servers/src/main/java/com/nitorcreations/willow/auth/PublicKeyAuthenticationFilter.java
+++ willow-servers/src/main/java/com/nitorcreations/willow/auth/PublicKeyAuthenticationFilter.java
@@ -1,5 +1,7 @@
 package com.nitorcreations.willow.auth;
 
+import java.util.TreeSet;
+
 import javax.inject.Singleton;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
@@ -13,7 +15,7 @@ import org.slf4j.LoggerFactory;
 @Singleton
 public class PublicKeyAuthenticationFilter extends BasicHttpAuthenticationFilter {
   private static final Logger log = LoggerFactory.getLogger(PublicKeyAuthenticationFilter.class);
-
+  private final TreeSet<Long> timeStamps = new TreeSet<>();
   private static final String scheme = "PUBLICKEY";
   public PublicKeyAuthenticationFilter() {
     this.setAuthcScheme(scheme);
@@ -34,8 +36,19 @@ public class PublicKeyAuthenticationFilter extends BasicHttpAuthenticationFilter
     if (header.length < 3 || !scheme.equals(header[0].toUpperCase())) {
       return createToken("", null, null, request);
     }
-    String unameNow = Base64.decodeToString(header[1]);
-    String username = unameNow.split(":")[0];
+    String[] unameNow = Base64.decodeToString(header[1]).split(":");
+    String username = unameNow[0];
+    long timestamp = Long.parseLong(unameNow[1]);
+    long currentTime = System.currentTimeMillis();
+    long wStart = currentTime - 15000;
+    long wEnd = currentTime + 15000;
+    if (!((timestamp > wStart) && (timestamp < wEnd)) || timeStamps.contains(timestamp)) {
+      return createToken("", null, null, request);
+    }
+    timeStamps.add(timestamp);
+    while (timeStamps.first() < wStart) {
+      timeStamps.pollFirst();
+    }
     PublicKeyAuthenticationToken token = createToken(username, Base64.decode(header[1]), Base64.decode(header[2]), request);
     for (int i=3; i<header.length;i++) {
       token.addSignature(Base64.decode(header[i]));
