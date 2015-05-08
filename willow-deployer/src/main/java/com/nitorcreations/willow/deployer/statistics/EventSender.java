7e60bf623ed08cb4bd22f0e912903c883cb1aadd
Mika Majakorpi <mika@majakorpi.net>
2015-05-08 10:47:45 +0300

    Dummy event sender for testing

---
+++ willow-deployer/src/main/java/com/nitorcreations/willow/deployer/statistics/EventSender.java
@@ -0,0 +1,37 @@
+package com.nitorcreations.willow.deployer.statistics;
+
+import com.nitorcreations.willow.messages.event.EventMessage;
+import com.nitorcreations.willow.messages.event.MetricThresholdEvent;
+
+import javax.inject.Named;
+import java.util.Properties;
+import java.util.logging.Logger;
+
+@Named("event")
+public class EventSender extends AbstractStatisticsSender {
+
+  private Logger logger = Logger.getLogger(getClass().getName());
+
+  @Override
+  public void execute() {
+    //note that this is just a test class that sends a hardcoded event...
+    MetricThresholdEvent mte = new MetricThresholdEvent();
+    mte.description = "event description";
+    mte.metric = "myMetric";
+    mte.threshold = 10D;
+    mte.value = 11D;
+    logger.finest("Sending event");
+
+    transmitter.queue(mte.getEventMessage());
+    try {
+      Thread.sleep(10000);
+    } catch (InterruptedException e1) {
+      e1.printStackTrace();
+    }
+  }
+
+  @Override
+  public void setProperties(Properties properties) {
+
+  }
+}
