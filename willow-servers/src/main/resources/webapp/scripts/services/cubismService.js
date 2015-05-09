d2c10d56795e797e2d6f7bacc995db28aa751e33
mlison <michal.lison@nitor.fi>
2015-05-09 23:59:24 +0300

    fixed cubism focus events so they don't override each other

--- willow-servers/src/main/resources/webapp/scripts/services/cubismService.js
+++ willow-servers/src/main/resources/webapp/scripts/services/cubismService.js
@@ -1,10 +1,21 @@
 Box.Application.addService('cubism-graphs', function(application) {
   'use strict';
 
-  var cubism = application.getGlobal('cubism');
+  var windowSvc = application.getService('window');
+  var cubism    = application.getGlobal('cubism');
 
-  var cubismContext;
+  var timescale = windowSvc.getHashVariable('timescale') || 10800;
 
+  var cubismContext = cubism.context();
+  var focusEvents   = [];
+
+  cubismContext.on('focus', function(index) {
+    focusEvents.forEach(function(eventHandler) {
+      eventHandler.call(cubismContext, index);
+    });
+  }, cubismContext);
+
+  // TODO: this may need debouncing, each graph will call this on it's reset
   function resetCubismContext(step, widthInPixels) {
     cubismContext.step(step)
         .size(widthInPixels);
@@ -15,7 +26,7 @@ Box.Application.addService('cubism-graphs', function(application) {
   var step      = parseInt(timescale * 1000 / widthInPx);
   resetCubismContext(step, widthInPx);
 
-  //TODO should we only wrap cubism context handling into this service
+  // TODO should we only wrap cubism context handling into this service
   // or should we set default configurations such as the axis layout?
   return {
     resetCubismContext: resetCubismContext,
@@ -26,7 +37,9 @@ Box.Application.addService('cubism-graphs', function(application) {
       return cubismContext.rule();
     },
     onFocus: function(eventHandler) {
-      cubismContext.on("focus", eventHandler, cubismContext);
+      // there can only be one focus event per context:
+      // event cache will take care of executing all events
+      focusEvents.push(eventHandler);
     },
     createMetrics: function(dataSourceRequest, name) {
       return cubismContext.metric(dataSourceRequest, name);
