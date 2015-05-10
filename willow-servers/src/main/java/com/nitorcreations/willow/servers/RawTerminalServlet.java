cf55eb7b4fc89d473ae842b66a23bc24f5cd377b
Pasi Niemi <pasi.niemi@nitorcreations.com>
2015-05-10 23:18:44 +0300

    Add permission check to terminal

--- willow-servers/src/main/java/com/nitorcreations/willow/servers/RawTerminalServlet.java
+++ willow-servers/src/main/java/com/nitorcreations/willow/servers/RawTerminalServlet.java
@@ -1,9 +1,14 @@
 package com.nitorcreations.willow.servers;
 
+import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
 import javax.inject.Singleton;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
+import org.apache.shiro.SecurityUtils;
 import org.eclipse.jetty.websocket.servlet.WebSocketServlet;
 import org.eclipse.jetty.websocket.servlet.WebSocketServletFactory;
 
@@ -13,6 +18,13 @@ import com.nitorcreations.willow.ssh.RawSecureShellWS;
 public class RawTerminalServlet extends WebSocketServlet {
   private static final long serialVersionUID = -7940037116569261919L;
 
+  @Override
+  protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    if (!SecurityUtils.getSubject().isPermitted("admin")) {
+      resp.sendError(HttpServletResponse.SC_FORBIDDEN);
+    }
+    super.service(req, resp);
+  };
   @Override
   public void configure(WebSocketServletFactory factory) {
     factory.getPolicy().setIdleTimeout(TimeUnit.MINUTES.toMillis(5));
