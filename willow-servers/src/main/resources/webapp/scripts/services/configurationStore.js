831923fff4fe2a9c966d1269712b5df0aa0add30
Sami Airaksinen <sami.airaksinen@nitorcreations.com>
2015-05-19 13:45:08 +0300

    scoped access to custom radiator configurations
    closes #17

--- willow-servers/src/main/resources/webapp/scripts/services/configurationStore.js
+++ willow-servers/src/main/resources/webapp/scripts/services/configurationStore.js
@@ -1,6 +1,8 @@
 Box.Application.addService('configuration-store', function(application) {
   'use strict';
 
+  var localStorage = application.getGlobal("localStorage");
+
   function readConfiguration(configId) {
     return JSON.parse(localStorage.getItem(configId));
   }
@@ -9,7 +11,31 @@ Box.Application.addService('configuration-store', function(application) {
     localStorage.setItem(configId, JSON.stringify(config));
   }
 
+  //FIXME each sub config area to own scope (custom, index, host)
+  function customRadiatorKey(radiatorId) {
+    return "willow:custom_radiators:" + radiatorId;
+  }
+
+  function readRadiatorConfiguration(radiatorName) {
+    return readConfiguration(customRadiatorKey(radiatorName));
+  }
+
+  function storeRadiatorConfiguration(radiatorName, config) {
+    return storeConfiguration(customRadiatorKey(radiatorName), config);
+  }
+
+  function appendGraphToRadiator(radiatorName, config) {
+    var existingConfig = readRadiatorConfiguration(radiatorName) || [];
+    existingConfig.push(config);
+    storeRadiatorConfiguration(radiatorName, existingConfig);
+  }
+
   return {
+    customRadiators: {
+      readConfiguration: readRadiatorConfiguration,
+      storeConfiguration: storeRadiatorConfiguration,
+      appendConfig: appendGraphToRadiator
+    },
     readConfiguration: readConfiguration,
     storeConfiguration: storeConfiguration
   };
