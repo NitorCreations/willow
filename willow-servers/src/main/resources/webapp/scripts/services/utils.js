0052484eb753010381351b29e3dd3950a866725c
mlison <michal.lison@nitor.fi>
2015-05-22 15:36:36 +0300

    added util.guid

--- willow-servers/src/main/resources/webapp/scripts/services/utils.js
+++ willow-servers/src/main/resources/webapp/scripts/services/utils.js
@@ -32,6 +32,16 @@ Box.Application.addService('utils', function(application) {
       el.innerText = el.textContent = str;
       str = el.innerHTML;
       return str;
+    },
+
+    // generates a new guid
+    guid: function() {
+      return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g,
+        function(c) {
+          var r = Math.random() * 16 | 0,
+            v = c == 'x' ? r : (r & 0x3 | 0x8);
+          return v.toString(16);
+        }).toUpperCase();
     }
   };
 });
