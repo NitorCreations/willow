6eb598e4ef63c416930f7369c3781d9eaceb7d64
Pasi Niemi <pasi.niemi@nitorcreations.com>
2015-05-25 00:10:44 +0300

    Add jquery and jquery ui as jspm dependencies and upgrade them. Also add jquery ui smoothness theme

--- willow-servers/update-javascript-dependencies.sh
+++ willow-servers/update-javascript-dependencies.sh
@@ -8,3 +8,5 @@ for lib in $(xpath -q -e "//group[@name='libs']/*/text()" src/main/resources/wro
     cp $src src/main/resources/webapp$lib
   fi
 done
+cp -a ./target/jspm_packages/github/components/jqueryui@*/themes/smoothness/images src/main/resources/webapp/styles/
+cp ./target/jspm_packages/github/components/jqueryui@*/themes/smoothness/jquery-ui.css src/main/resources/webapp/styles/
