ed42b33c508c5b33fea8e4bcf39db5735a7f2068
Sami Airaksinen <samiaira@gmail.com>
2015-06-01 21:56:46 +0300

    rewrote tests for configuration store

--- willow-servers/src/test/javascript/services/test-configuration-store.js
+++ willow-servers/src/test/javascript/services/test-configuration-store.js
@@ -1,6 +1,5 @@
 describe("Tests for configuration store:", function() {
-  //TODO resolve issue with mocking local storage in tests
-  var service, sandbox, contextFake, mockStorage;
+  var service, sandbox, contextFake;
 
   var radiatorConfigKey = "willow:custom_radiators";
 
@@ -28,53 +27,62 @@ describe("Tests for configuration store:", function() {
   };
 
   beforeEach(function() {
-    localStorage.clear();
     contextFake = new Box.TestServiceProvider();
     service = Box.Application.getServiceForTest('configuration-store', contextFake);
     sandbox = sinon.sandbox.create();
-    sandbox.mock(localStorage).expects('getItem').withExactArgs(radiatorConfigKey)
-        .returns(JSON.stringify(existingConfigurations));
+    var localStorageGetItemStubbed = sandbox.stub(window.localStorage, "getItem");
+    localStorageGetItemStubbed.withArgs(radiatorConfigKey).returns(JSON.stringify(existingConfigurations));
   });
 
-  it('reads existing radiator configurations from proper place', function() {
-    service.customRadiators.listAvailableRadiators();
+  it('lists available radiator names', function() {
+    expect(service.customRadiators.listAvailableRadiators()).toEqual(['rad1','rad2']);
   });
 
-  it('reads existing radiator configuration', function() {
-    service.customRadiators.readConfiguration("rad1");
+  it('returns existing radiator configuration', function() {
+    expect(service.customRadiators.readConfiguration("rad1")).toEqual([testGraphConfig2]);
   });
 
-  /*
-   //FIXME figure out why this expectation does not work
   it('append graph configuration in existing radiator config', function() {
-    var expectedConfiguration = existingConfigurations.rad1.push(testGraphConfig);
+    var expectedConfigurationsAfterAppend = {
+      rad1: [testGraphConfig2, testGraphConfig],
+      rad2: [testGraphConfig2, testGraphConfig3]
+    };
     sandbox.mock(localStorage).expects('setItem')
-        .withExactArgs(radiatorConfigKey, JSON.stringify(expectedConfiguration));
+      .withExactArgs(
+      radiatorConfigKey,
+      JSON.stringify(expectedConfigurationsAfterAppend)
+    );
     service.customRadiators.appendConfiguration("rad1", testGraphConfig);
   });
 
-/*
-  //FIXME figure out why this expectation does not work
   it('append graph configuration with new radiator key', function() {
-    var expectedConfiguration = existingConfigurations;
-    expectedConfiguration["new_rad"] = [testGraphConfig];
+    var expectedConfigurationsAfterAppend = {
+      'rad1': [testGraphConfig2],
+      'rad2': [testGraphConfig2, testGraphConfig3],
+      'new-rad': [testGraphConfig]
+    };
     sandbox.mock(localStorage).expects('setItem')
-        .withExactArgs(radiatorConfigKey, JSON.stringify(expectedConfiguration));
+        .withExactArgs(
+      radiatorConfigKey,
+      JSON.stringify(expectedConfigurationsAfterAppend)
+    );
     service.customRadiators.appendConfiguration("new-rad", testGraphConfig);
   });
 
-  //FIXME figure out why this expectation does not work
-  it('store graph configuration', function() {
-    var expectedConfiguration = existingConfigurations;
-    expectedConfiguration.rad2 = [testGraphConfig];
+  it('overwrite radiator configuration', function() {
+    var expectedConfigurationsAfterAppend = {
+      'rad1': [testGraphConfig2],
+      'rad2': [testGraphConfig]
+    };
     sandbox.mock(localStorage).expects('setItem')
-        .withExactArgs(radiatorConfigKey, JSON.stringify(expectedConfiguration));
+        .withExactArgs(
+      radiatorConfigKey,
+      JSON.stringify(expectedConfigurationsAfterAppend)
+    );
     service.customRadiators.storeConfiguration("rad2", [testGraphConfig]);
   });
-*/
 
   afterEach(function() {
-    localStorage.clear();
     sandbox.verifyAndRestore();
   });
 });
