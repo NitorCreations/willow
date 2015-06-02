8e1a39837879f66e5f18d3ddc4eb7bf14259b1a8
Sami Airaksinen <samiaira@gmail.com>
2015-06-02 21:32:31 +0300

    Test configuration store operations

--- willow-servers/src/test/javascript/services/test-configuration-store.js
+++ willow-servers/src/test/javascript/services/test-configuration-store.js
@@ -10,12 +10,14 @@ describe("Tests for configuration store:", function() {
   };
 
   var testGraphConfig2 = {
+    id: 1,
     host: "test-host2",
     instaceTag: "test-instances2",
     metric: "test-metric2"
   };
 
   var testGraphConfig3 = {
+    id:3,
     host: "test-host3",
     instaceTag: "test-instances3",
     metric: "test-metric3"
@@ -43,8 +45,10 @@ describe("Tests for configuration store:", function() {
   });
 
   it('append graph configuration in existing radiator config', function() {
+    var insertedGraphConfig = clone(testGraphConfig);
+    insertedGraphConfig.id = 2;
     var expectedConfigurationsAfterAppend = {
-      rad1: [testGraphConfig2, testGraphConfig],
+      rad1: [testGraphConfig2, insertedGraphConfig],
       rad2: [testGraphConfig2, testGraphConfig3]
     };
     sandbox.mock(localStorage).expects('setItem')
@@ -56,10 +60,12 @@ describe("Tests for configuration store:", function() {
   });
 
   it('append graph configuration with new radiator key', function() {
+    var insertedGraphConfig = clone(testGraphConfig);
+    insertedGraphConfig.id = 1;
     var expectedConfigurationsAfterAppend = {
       'rad1': [testGraphConfig2],
       'rad2': [testGraphConfig2, testGraphConfig3],
-      'new-rad': [testGraphConfig]
+      'new-rad': [insertedGraphConfig]
     };
     sandbox.mock(localStorage).expects('setItem')
         .withExactArgs(
@@ -82,7 +88,36 @@ describe("Tests for configuration store:", function() {
     service.customRadiators.storeConfiguration("rad2", [testGraphConfig]);
   });
 
+  it('removes radiator configuration', function() {
+    var expectedConfigurationsAfterAppend = {
+      'rad1': [testGraphConfig2]
+    };
+    sandbox.mock(localStorage).expects('setItem')
+      .withExactArgs(
+      radiatorConfigKey,
+      JSON.stringify(expectedConfigurationsAfterAppend)
+    );
+    service.customRadiators.removeConfiguration("rad2");
+  });
+
+  it('removes graph configuration from radiator', function() {
+    var expectedConfigurationsAfterAppend = {
+      'rad1': [testGraphConfig2],
+      'rad2': [testGraphConfig2]
+    };
+    sandbox.mock(localStorage).expects('setItem')
+      .withExactArgs(
+      radiatorConfigKey,
+      JSON.stringify(expectedConfigurationsAfterAppend)
+    );
+    service.customRadiators.removeRadiatorConfig("rad2", testGraphConfig3);
+  });
+
   afterEach(function() {
     sandbox.verifyAndRestore();
   });
+
+  function clone(object) {
+    return JSON.parse(JSON.stringify(object));
+  }
 });
