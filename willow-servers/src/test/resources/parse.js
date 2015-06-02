8c56ac4526c6b33f4706b7ebd940b9bbd7a5dc74
Pasi Niemi <pasi.niemi@nitorcreations.com>
2015-06-02 17:57:41 +0300

    Add finding services and globals

--- willow-servers/src/test/resources/parse.js
+++ willow-servers/src/test/resources/parse.js
@@ -11,6 +11,10 @@ for (var i=0; i<body.length;i++) {
     var moduleFunction = body[i].expression.arguments[1];
     boxName = moduleFunction.params[0].name;
     var moduleBody = moduleFunction.body.body;
+    console.log("globals: " +
+      JSON.stringify(findGlobals(moduleFunction)));
+    console.log("services: " +
+      JSON.stringify(findServices(moduleFunction)));
     for (var j=0;j<moduleBody.length;j++) {
       if (moduleBody[j].type == "FunctionDeclaration") {
         console.log("private " + moduleBody[j].id.name);
@@ -53,19 +57,39 @@ function traverse(node, func) {
 
 function findBroadcast(functionNode) {
   var broadcasts = [];
-  traverse(functionNode, function(nextNode) {
-    if (nextNode.expression &&
-      nextNode.expression.callee &&
-      nextNode.expression.callee.object &&
-      nextNode.expression.callee.object.name &&
-      nextNode.expression.callee.object.name == boxName &&
-      nextNode.expression.callee.property &&
-      nextNode.expression.callee.property.name &&
-      nextNode.expression.callee.property.name == "broadcast")
-        broadcasts.push({
-          message: nextNode.expression.arguments[0].value,
-          data: nextNode.expression.arguments[1] ? escodegen.generate(nextNode.expression.arguments[1]) : null
-        });
-  });
+  traverse(functionNode, findBoxFunc(boxName, "broadcast", function(arguments) {
+    broadcasts.push({
+      message: arguments[0].value,
+      data: arguments[1] ? escodegen.generate(arguments[1]) : null
+    });
+  }));
   return broadcasts;
+}
+function findGlobals(functionNode) {
+  var globals = [];
+  traverse(functionNode, findBoxFunc(boxName, "getGlobal", function(arguments) {
+    globals.push(arguments[0].value);
+  }));
+  return globals;
+}
+function findServices(functionNode) {
+  var services = [];
+  traverse(functionNode, findBoxFunc(boxName, "getService", function(arguments) {
+    services.push(arguments[0].value);
+  }));
+  return services;
+}
+function findBoxFunc(boxName, funcName, callback) {
+  return function(nextNode) {
+    if (nextNode &&
+      nextNode.callee &&
+      nextNode.callee.object &&
+      nextNode.callee.object.name &&
+      nextNode.callee.object.name == boxName &&
+      nextNode.callee.property &&
+      nextNode.callee.property.name &&
+      nextNode.callee.property.name == funcName) {
+        callback(nextNode.arguments);
+    }
+  };
 }
