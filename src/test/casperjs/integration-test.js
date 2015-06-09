f50e2c94e8e4cddad5b4fbd446360c49087ebc30
Pasi Niemi <pasi.niemi@nitorcreations.com>
2015-06-09 12:56:16 +0300

    Added istanbul for integration test coverage

--- src/test/casperjs/integration-test.js
+++ src/test/casperjs/integration-test.js
@@ -2,26 +2,38 @@ var root = casper.cli.options.home;
 casper.options.viewportSize = { width: 1920, height: 1080 };
 casper.start(root);
 casper.setHttpAuth('admin', 'admin');
+casper.viewport(1920, 1080);
+var fs = require('fs');
+
+var cpuLink = { type: 'xpath', path: '//a[@id="cpu"]' };
 var hostLink = { type: 'xpath', path: '//a[@data-host="integrationtest"]' };
 var heapDiv = '#mod-heap-graph-1';
-casper.viewport(1920, 1080);
-casper.test.begin('integrationtest cpu graph is shown', 2, function(test) {
+var getCoverage = function() { return window.__coverage__; };
+var writeCoverage = function(cspr, covnum) {
+  var coverage = cspr.evaluate(getCoverage);
+  if (coverage) {
+    fs.write("target/js-coverage/test" + covnum + ".json",
+      JSON.stringify(coverage), 'w');
+  }
+}
+casper.test.begin('navigate to host page', 2, function(test) {
   casper.start(root + "/#metric=cpu&timescale=10800", function() {
-    test.assertExists({ type: 'xpath', path: '//a[@id="cpu"]' });
+    test.assertExists(cpuLink);
     this.waitUntilVisible(hostLink,
       function() {
         this.click(hostLink);
         casper.waitForPopup(/radiator\.html/);
         casper.withPopup(/radiator\.html/, function() {
           this.waitUntilVisible(heapDiv, function() {
-            this.viewport(1920, 1080);
             this.capture("screenshot.png");
+            writeCoverage(this, 1);
             test.pass('integrationtest graph visible');
           });
         });
+        writeCoverage(casper, 2);
       },
       function() {
-        //this.capture("screenshot.png");
+        this.capture("failed-screenshot.png");
       }, 15000);
   }).run(function() {
     test.done();
