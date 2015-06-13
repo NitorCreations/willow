e2de400cb847c79b632a52442ceb8af736ea975f
Pasi Niemi <pasi.niemi@nitorcreations.com>
2015-06-13 23:14:04 +0300

    Move integration tests to it's own module so we can run everything in one pass

--- willow-integrationtests/src/test/resources/integration-tests.sh
+++ willow-integrationtests/src/test/resources/integration-tests.sh
@@ -6,8 +6,8 @@ on_error() {
   echo $1
   exit 1
 }
-
-if ! [ -x willow-deployer/target/deployer.sh ]; then
+DEPLOYER=../willow-deployer/target/deployer.sh
+if ! [ -x $DEPLOYER ]; then
   on_error "Deployer script not found"
 fi
 
@@ -28,16 +28,16 @@ ssh-add src/test/resources/id_rsa
 
 JACOCO_PREFIX="-javaagent:target/jacoco-agent.jar=jmx=true,destfile=target/"
 export W_JAVA_OPTS="-Dserver.port=$SERVER_PORT "$JACOCO_PREFIX"willow-deployer/run-its.exec"
-bash -x willow-deployer/target/deployer.sh start integration-test file:src/test/resources/integration-test.properties &
+bash -x $DEPLOYER start integration-test file:src/test/resources/integration-test.properties &
 sleep 60
 casperjs test --verbose --no-colors --concise --home=http://localhost:$SERVER_PORT src/test/casperjs/suites
 
 export W_JAVA_OPTS=$JACOCO_PREFIX"willow-deployer/status.exec"
-willow-deployer/target/deployer.sh status integration-test
+$DEPLOYER status integration-test
 export W_JAVA_OPTS=$JACOCO_PREFIX"willow-deployer/jmxoperation.exec"
-willow-deployer/target/deployer.sh jmxoperation integration-test metrics_server "org.eclipse.jetty.util.thread:type=queuedthreadpool,id=0" threads
+$DEPLOYER jmxoperation integration-test metrics_server "org.eclipse.jetty.util.thread:type=queuedthreadpool,id=0" threads
 export W_JAVA_OPTS=$JACOCO_PREFIX"willow-deployer/stop.exec"
-willow-deployer/target/deployer.sh stop integration-test
+$DEPLOYER stop integration-test
 
 if [ -n "$AGENT_STARTED" ]; then
   kill "$SSH_AGENT_PID"
