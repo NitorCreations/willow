68cdaac4e060827e0b309b2146a97a9ef05f9939
Pasi Niemi <pasi.niemi@nitorcreations.com>
2015-07-02 20:25:53 +0300

    Add an id into polling so that several clients can register for events in the same websocket

--- willow-servers/src/main/java/com/nitorcreations/willow/servers/ServerSidePollingSocket.java
+++ willow-servers/src/main/java/com/nitorcreations/willow/servers/ServerSidePollingSocket.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
@@ -24,6 +25,8 @@ import com.google.inject.Injector;
 import com.nitorcreations.willow.messages.metrics.MetricConfig;
 import com.nitorcreations.willow.metrics.Metric;
 
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+
 @WebSocket
 @Named
 public class ServerSidePollingSocket extends BasicWillowSocket {
@@ -35,6 +38,11 @@ public class ServerSidePollingSocket extends BasicWillowSocket {
   private List<ScheduledFuture<?>> pollers = new ArrayList<>();
   public final int minStep = 1000;
   private final Gson gson = new Gson();
+  @SuppressFBWarnings(value={"URF_UNREAD_PUBLIC_OR_PROTECTED_FIELD"}, justification="Fields used in serialization")
+  private static class PollResponse {
+    public String id;
+    public Object data;
+  }
   private class PollTask implements Runnable {
     private final Metric metric;
     private final MetricConfig conf;
@@ -59,10 +67,12 @@ public class ServerSidePollingSocket extends BasicWillowSocket {
         long start = stop - conf.getStep();
         Metric nextMetric = metric.getClass().newInstance();
         injector.injectMembers(nextMetric);
-        Object ret = nextMetric.calculateMetric(conf);
+        PollResponse resp = new PollResponse();
+        resp.id = conf.getId();
+        resp.data = nextMetric.calculateMetric(conf);
         conf.setStop(stop);
         conf.setStart(start);
-        session.getRemote().sendString(gson.toJson(ret));
+        session.getRemote().sendString(gson.toJson(resp));
       } catch (InstantiationException | IllegalAccessException | IOException e) {
         log.log(Level.INFO, "Failed to send polled statistics", e);
       }
@@ -77,6 +87,9 @@ public class ServerSidePollingSocket extends BasicWillowSocket {
   public void messageReceived(Session session, String message) {
     try {
       MetricConfig conf = gson.fromJson(message, MetricConfig.class);
+      if (conf.getId() == null || conf.getId().isEmpty()) {
+        conf.setId(UUID.randomUUID().toString());
+      }
       PollTask task = new PollTask(session, conf);
       ScheduledFuture<?> handle = scheduler.scheduleAtFixedRate(task, 0, conf.getStep(), TimeUnit.MILLISECONDS);
       pollers.add(handle);
