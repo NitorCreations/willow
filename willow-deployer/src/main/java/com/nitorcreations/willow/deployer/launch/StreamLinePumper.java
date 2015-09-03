a785d9192932c3e7a4f29d1852673c9940d42c19
Pasi Niemi <pasi.niemi@nitorcreations.com>
2015-09-03 17:49:23 +0300

    In addition to transmitting stdout to metrics server, log it so that it gets printed into system log

--- willow-deployer/src/main/java/com/nitorcreations/willow/deployer/launch/StreamLinePumper.java
+++ willow-deployer/src/main/java/com/nitorcreations/willow/deployer/launch/StreamLinePumper.java
@@ -2,6 +2,9 @@ package com.nitorcreations.willow.deployer.launch;
 
 import java.io.InputStream;
 import java.nio.charset.Charset;
+import java.util.logging.Level;
+import java.util.logging.LogRecord;
+import java.util.logging.Logger;
 
 import com.nitorcreations.willow.messages.LogMessage;
 import com.nitorcreations.willow.messages.WebSocketTransmitter;
@@ -9,15 +12,18 @@ import com.nitorcreations.willow.utils.AbstractStreamPumper;
 
 class StreamLinePumper extends AbstractStreamPumper {
   private final WebSocketTransmitter transmitter;
-
+  private final Logger log;
   public StreamLinePumper(InputStream in, WebSocketTransmitter transmitter, String name, Charset charset) {
     super(in, name, charset);
+    log = Logger.getLogger(name);
     this.transmitter = transmitter;
   }
 
   @Override
   public void handle(String line) {
-    LogMessage msg = new LogMessage(System.currentTimeMillis(), getName(), line);
+    LogRecord rec = new LogRecord(Level.INFO, line);
+    LogMessage msg = new LogMessage(rec);
+    log.log(rec);
     transmitter.queue(msg);
   }
 }
